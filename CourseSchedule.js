78969f55b08567b9b88389fc5709d821f0f3a81d
da5idf <davidpforster24@gmail.com>
2022-08-02 15:48:45 -0400

    CourseSchedule, Merge2SortedArrsInPlace

--- CourseSchedule.js
+++ CourseSchedule.js
@@ -1,33 +1,60 @@
 class Node {
-    constructor(val, next) {
+    constructor(val) {
         this.val = val;
-        this.next = next;
+        this.next = [];
     }
 }
 
 var canFinish = function (numCourses, prerequisites) {
+
+    let V = new Array(numCourses - 1).fill(false);
 
     for (let i = 0; i < prerequisites.length; i++) {
         let prereq = prerequisites[i];
         let nextClass = prereq[0]
         let reqClass = prereq[1]
 
-        const nextNode = new Node(nextClass, null)
-        const reqNode = new Node(reqClass, reqClass)
+        if (!V[nextClass]) {
+            const nextNode = new Node(nextClass)
+            V[nextClass] = nextNode;
+        }
+        if (!V[reqClass]) {
+            const reqNode = new Node(reqClass);
+            V[reqClass] = reqNode;
+        }
+        // add the next class to the required class's next list
+        // i.e. after taking reqNode, you are able to take nextNode
+        V[reqClass].next.push(V[nextClass])
     }
+
+    for (let i = 0; i < numCourses; i++) {
+        // if V[i] === false then that course has no pre-reqs, continue
+        if (!V[i]) continue;
+
+        if (!dfs(V[i])) return false;
+    }
+    return true;
 }
 
-var hasCycle = function (head) {
+var dfs = function (head) {
 
-    if (!head) return false;
+    let queue = head.next;
+    let visited = new Set();
 
-    let slow = head;
-    let fast = head.next;
+    while (queue.length) {
+        let current = queue.shift();
+        visited.add(current.val);
 
-    while (fast && fast.next) {
-        if (slow === fast) return true
-        slow = slow.next;
-        fast = fast.next.next
+        // if current is the head, then we have a cycle
+        if (current.val === head.val) return false;
+        // add each next to the queue if we haven't visited that node yet.
+        let adjList = current.next;
+        adjList.forEach(node => {
+            if (!visited.has(node.val)) {
+                queue.push(node);
+            }
+        })
     }
-    return false
+
+    return true
 }
