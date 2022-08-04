fc7be87efe7bcf82cde88ed9535ddf5186d399d4
da5idf <davidpforster24@gmail.com>
2022-08-04 16:59:52 -0400

    cleaner RottenOranges

--- Review/RottenOranges.js
+++ Review/RottenOranges.js
@@ -3,83 +3,41 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
-    let [rottens, oranges] = traverse(grid);
-
-    rottens.forEach(rotten => {
-        dfs(rotten, grid, oranges);
-    })
-
-    let distances = Object.values(oranges);
-    distances.sort((a, b) => a - b);
-
-    if (!distances.length) return 0;
-    if (distances[0] === -1) return -1
-    return distances[distances.length - 1];
-};
-
-var traverse = function (grid) {
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
     let rottens = [];
-    let oranges = {};
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if (grid[i][j] === 2) {
-                rottens.push([i, j]);
-                oranges[`${i}${j}`] = 0;
+    let numFresh = 0;
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (grid[r][c] === 2) rottens.push([r, c]);
+            if (grid[r][c] === 1) numFresh++;
+        }
+    }
+
+    let time = 0;
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    while (rottens.length && numFresh > 0) {
+
+        // add this for loop to keep track of time.
+        let len = rottens.length
+        // this way we only increment time after all neighbors of current
+        // round have been explored.
+        for (let i = 0; i < len; i++) {
+            let [r, c] = rottens.shift();
+            for (let [row, col] of directions) {
+                row = row + r;
+                col = col + c;
+
+                // check boundry and ignore 2's and 0's
+                if (row < 0 || row === ROWS || col < 0 || col === COLS || grid[row][col] !== 1) continue;
+
+                rottens.push([row, col])
+                grid[row][col] = 2;
+                numFresh--;
             }
-            if (grid[i][j] === 1) oranges[`${i}${j}`] = -1;
         }
-    }
-    return [rottens, oranges]
-}
-
-var dfs = function (start, grid, oranges) {
-    let visited = new Set();
-    let queue = [[start, 0]];
-
-    while (queue.length) {
-        let [current, depth] = queue.shift();
-        visited.add(`${current[0]}${current[1]}`);
-
-        addNeighbors(current, depth, queue, visited, grid, oranges);
-    }
-}
-
-var addNeighbors = function ([row, col], depth, queue, visited, grid, oranges) {
-    if (row > 0 && grid[row - 1][col] === 1 && !visited.has(`${row - 1}${col}`)) {
-        queue.push([[row - 1, col], depth + 1])
-        if (oranges[`${row - 1}${col}`] === -1) {
-            oranges[`${row - 1}${col}`] = depth + 1
-        } else {
-            oranges[`${row - 1}${col}`] = Math.min(depth + 1, oranges[`${row - 1}${col}`])
-        }
+        time++;
     }
 
-    if (col < grid[0].length && grid[row][col + 1] === 1 && !visited.has(`${row}${col + 1}`)) {
-        queue.push([[row, col + 1], depth + 1])
-        if (oranges[`${row}${col + 1}`] === -1) {
-            oranges[`${row}${col + 1}`] = depth + 1;
-        } else {
-            oranges[`${row}${col + 1}`] = Math.min(depth + 1, oranges[`${row}${col + 1}`])
-        }
-    }
-
-    if (row < grid.length - 1 && grid[row + 1][col] === 1 && !visited.has(`${row + 1}${col}`)) {
-        queue.push([[row + 1, col], depth + 1])
-        if (oranges[`${row + 1}${col}`] === -1) {
-            oranges[`${row + 1}${col}`] = depth + 1
-        } else {
-            oranges[`${row + 1}${col}`] = Math.min(depth + 1, oranges[`${row + 1}${col}`])
-
-        }
-    }
-
-    if (col > 0 && grid[row][col - 1] === 1 && !visited.has(`${row}${col - 1}`)) {
-        queue.push([[row, col - 1], depth + 1])
-        if (oranges[`${row}${col - 1}`] === -1) {
-            oranges[`${row}${col - 1}`] = depth + 1
-        } else {
-            oranges[`${row}${col - 1}`] = Math.min(depth + 1, oranges[`${row}${col - 1}`])
-
-        }
-    }
-}
+    return numFresh === 0 ? time : -1
+};
