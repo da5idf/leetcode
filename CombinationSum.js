4ea55d3ca4e422d43b959ccad2fb5a0ae0c8450d
da5idf <davidpforster24@gmail.com>
2022-08-17 09:56:24 -0400

    Combination Sum fix

--- CombinationSum.js
+++ CombinationSum.js
@@ -36,11 +36,9 @@ var combinationSum = function (candidates, target) {
             }
         }
     }
-    console.log(mapping)
-
-    return mapping[target].sums || -1
+    return mapping[target] ? mapping[target].sums : []
 };
 
-let candidates = [2, 3, 6, 7], target = 7
+let candidates = [2], target = 7
 
 console.log(combinationSum(candidates, target))
