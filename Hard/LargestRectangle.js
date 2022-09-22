a158ebd72481997f49a3bafc4d0b9f1530a3e02f
da5idf <davidpforster24@gmail.com>
2022-09-22 12:45:15 -0400

    largest rectangle

---
+++ Hard/LargestRectangle.js
@@ -0,0 +1,66 @@
+/***
+ * In this algorithm, we calculate rectangles backwards in a sense.
+ * Specifically, we don't calculate an area until we reach an index in heights
+ * where heights[i] < heights[j] and j < i (a decrease in height, but increase in idx). 
+ * 
+ * Until this point (where a decrease happens), we have been increasing in height by design.
+ *      We add to our stack during this period. Each element in the stack has 2 elements:
+ *      stack[i] = [leftBorder, heights[leftBorder]]
+ * 
+ * Also, by design the first rectangle we calculate is for j = i - 1.
+ * Notice how the width of the rectangle is
+ *      width = i - j = i - (i - 1) = 1. 
+ * The key here is that we do NOT do i - j + 1. Ex. 
+ *      heights = [6, 2] => i = 1, j = i - 1 = 0. So width is i - j = 1 - 0 = 1
+ * We have designed the problem to correct for 0 indexing.
+ * 
+ * Each time we calculate a rectangle, we  j and update the current idx's (i's)
+ * leftBorder (newLeft) to be equal j.
+ * Why? Because we know that given height[j] > height[i] the current height (height[i]) can 
+ * span backwards, to the left up to and including index j. Ex
+ *      heights = [1, 3, 3, 4, 5, 2, 0]. When i = 5, heights[i] = 2. 
+ *      Note, we do NOT calculate 2's rectangle until i = 6. 
+ *      HOWEVER, at this stage, rectangles for idexes 1, 2, 3, and 4 are calculated.
+ *      At each index, we know that height[5] = 2 can span backward to these indexes. For instance
+ *      at index 1, height = 3, so for height = 2, we know leftBorder = 1.
+ *      leftBorder !== 0 because height[0] = 1 which is lower than 2.
+ */
+
+var largestRectangleArea = function (heights) {
+    heights.push(0)
+    let stack = [];
+    let maxArea = 0;
+    for (let rightBorder = 0; rightBorder < heights.length; rightBorder++) {
+        let newLeft = rightBorder;
+        while (stack.length && stack[stack.length - 1][1] > heights[rightBorder]) {
+            const [leftBorder, height] = stack.pop();
+            maxArea = Math.max(maxArea, (rightBorder - leftBorder) * height);
+            newLeft = leftBorder;
+        }
+        stack.push([newLeft, heights[rightBorder]]);
+    }
+    return maxArea;
+}
+
+
+/*
+Worst case n^2 time, so not great
+
+var largestRectangleArea = function (heights) {
+
+    var calculateArea = function (start, end) {
+        if (start > end) return 0;
+
+        let minIdx = start;
+        for (let i = start; i <= end; i++) {
+            if (heights[i] < heights[minIdx]) {
+                minIdx = i;
+            }
+        }
+
+        let currentLargest = heights[minIdx] * (end - start + 1)
+        return Math.max(currentLargest, calculateArea(0, minIdx - 1), calculateArea(minIdx + 1, end))
+    }
+    return calculateArea(0, heights.length - 1)
+}
+*/
