de3b1fecf17d18972ae81835821862924d8df8f0
da5idf <davidpforster24@gmail.com>
2022-09-27 14:20:59 -0400

    alternative pascals triangle

--- PascalsTriangle.js
+++ PascalsTriangle.js
@@ -1,4 +1,16 @@
 var generate = function (numRows) {
+    let pascal = [];
+    for (let i = 0; i < numRows; i++) {
+        pascal[i] = [1];
+        for (let j = 1; j < i; j++) {
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+        }
+        pascal[i][i] = 1
+    }
+    return pascal;
+}
+
+var generate = function (numRows) {
     let rows = [[1], [1, 1]]
     if (numRows === 1) return [[1]]
     else if (numRows === 2) return rows
