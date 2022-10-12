47d6ea2030f50f9bf4d9d75165ab11ce33e62294
da5idf <davidpforster24@gmail.com>
2022-10-12 18:15:38 -0400

    alternate solution first missing pos

--- Hard/FirstMissingPositive.js
+++ Hard/FirstMissingPositive.js
@@ -1,3 +1,38 @@
+// alternate method
+var firstMissingPositive = function (nums) {
+
+    // largetst possible missing number is nums.length.
+    // to give a nums.length idx, push an additional 0 into nums.
+    nums.push(0);
+    const LEN = nums.length;
+
+    // we need to ignore all negative numbers AND nums >= new length
+    for (let i = 0; i < LEN; i++) {
+        if (nums[i] < 0 || nums[i] >= LEN) {
+            nums[i] = 0;
+        }
+    }
+
+    // now each num in nums satisfies 0 <= num < LEN
+    // let each num serve as an idx.
+    // when we reach say nums[9] = 4 we go to nums[4] and add LEN
+    // nums[4] also satisfies line 16. So now it will be AT LEAST LEN
+    // in other words, each idx in nums will be greater than LEN if 
+    // that number was seen in LEN
+    // We take nums[i] % LEN so that we get the original number before
+    // we added anyhing to it during this loop.
+    for (let i = 0; i < LEN; i++) {
+        nums[nums[i] % LEN] += LEN
+    }
+
+    for (let i = 1; i < LEN; i++) {
+        if (nums[i] / LEN < 1) return i
+    }
+
+    return LEN
+}
+
+/*
 var firstMissingPositive = function (nums) {
     let foundOne = false;
     for (let i = 0; i < nums.length; i++) {
@@ -21,3 +56,4 @@ var firstMissingPositive = function (nums) {
 
     return nums.length + 1
 }
+*/
