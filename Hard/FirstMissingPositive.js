2ff166cd3c07ee85b3b0a98bd473ec03524b4094
da5idf <davidpforster24@gmail.com>
2022-10-12 15:17:41 -0400

    first missing pos integer

--- Hard/FirstMissingPositive.js
+++ Hard/FirstMissingPositive.js
@@ -1,3 +1,23 @@
 var firstMissingPositive = function (nums) {
+    let foundOne = false;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) foundOne = true;
+        else if (nums[i] < 1) nums[i] = 1;
+    }
 
+    if (!foundOne) return 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        const curNum = Math.abs(nums[i])
+        if (curNum <= nums.length) {
+            nums[curNum - 1] *= -1
+        }
+
+    }
+    console.log(nums);
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > 0) return i + 1
+    }
+
+    return nums.length + 1
 }
