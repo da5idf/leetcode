bf8d4e4a43450ed83a5520a4799ea33e7f973ee3
da5idf <davidpforster24@gmail.com>
2022-10-14 13:55:18 -0400

    delete node linked list

--- Medium/AddRowToTree.js
+++ Medium/AddRowToTree.js
@@ -1,15 +1,15 @@
 var addOneRow = function (root, val, depth) {
 
-    const queue = [[root, "left"]];
+    const queue = [root];
     let curDepth = 1;
     let prevLevel = []
     while (curDepth < depth) {
         let curLength = queue.length
         prevLevel = [...queue];
         for (let i = 0; i < curLength; i++) {
-            let node = queue.shift()[0];
-            if (node.left) queue.push([node.left, "left"]);
-            if (node.right) queue.push([node.right, "right"]);
+            let node = queue.shift();
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
         }
         curDepth++
     }
@@ -19,7 +19,7 @@ var addOneRow = function (root, val, depth) {
         newRoot.left = root;
         return newRoot
     } else {
-        prevLevel.forEach(([node, direction]) => {
+        prevLevel.forEach((node) => {
 
             const newLeft = new TreeNode(val)
             const curLeft = node.left;
--- Medium/DeleteNodeInLinkedList.js
+++ Medium/DeleteNodeInLinkedList.js
@@ -1,4 +1,6 @@
 var deleteNode = function (node) {
-
-
+    const nextNode = node.next;
+    node.val = nextNode.val;
+    node.next = nextNode.next;
+    nextNode.next = null;
 };
