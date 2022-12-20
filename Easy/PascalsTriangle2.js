3e386a9f0b6d54b0a1bc2cbff584047f3f42350a
da5idf <devda5idf@gmail.com>
2022-12-20 18:33:21 -0500

    pascal 2 deep dive into the math

--- Easy/PascalsTriangle2.js
+++ Easy/PascalsTriangle2.js
@@ -1,9 +1,32 @@
-// combinatorial method
+/***
+ * Each coefficient is a binomial coefficient.
+ * for row 8
+ *      [1,       8,     28,    56,   70,    56,    28,     8,     1]
+ *      [(8c0), (8c1), (8c2), (8c3), (8c4), (8c5), (8c6), (8c7), (8c8)]
+ * For loop:
+ *  i = 1, factorialBuilder = 8
+ *      row[1] = row[i - 1] * factorialBuilder / i
+ *      row[1] = (8c0) * 8 / 1
+ *      row[1] = 1 * 8 / 1
+ *      row[1] = 8 = (8c1) / 1
+ *  i = 2, factorialBuilder = 7
+ *      row[1] = row[i - 1] * factorialBuilder / i
+ *      row[1] = [(8c1) / 1] * 7 / 2
+ *      row[1] = 8 * 7 / 2
+ *      row[1] = 28 = (8c2)
+ *  i = 3, factorialBuilder = 6
+ *      row[1] = row[i - 1] * factorialBuilder / i
+ *      row[1] = (8c2) * 6 / 3
+ *      row[1] = (8 * 7 / 2) * 6 / 3
+ *      row[1] = (8 * 7 * 6) / (3 * 2)
+ *      row[1] = 56 = (8c3)
+ */
 var getRow = function (rowIdx) {
     var row = new Array(rowIdx + 1)
     row[0] = row[rowIdx] = 1
-    for (i = 1, up = rowIdx; i < rowIdx; i++, up--)
-        row[i] = row[i - 1] * up / i
+    let factorialBuilder = rowIdx
+    for (i = 1, factorialBuilder; i < factorialBuilder; i++, factorialBuilder--)
+        row[i] = row[factorialBuilder - 1] = row[i - 1] * factorialBuilder / i
     return row
 };
 
