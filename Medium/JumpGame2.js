5aee102518837ed9bbaa8ac56c704a411250ba71
da5idf <devda5idf@gmail.com>
2023-02-08 11:44:54 -0500

    jump game 2 alternate solution

--- Medium/JumpGame2.js
+++ Medium/JumpGame2.js
@@ -1,3 +1,20 @@
+// I found a different greedy approach I like better:
+var jump = function (nums) {
+    const n = nums.length;
+    let i = 0, maxReachable = 0, lastJumpedPos = 0, jumps = 0;
+    while (lastJumpedPos < n - 1) {
+        maxReachable = Math.max(maxReachable, i + nums[i]);
+        if (i === lastJumpedPos) {// current level has been iterated & maxReachable position on next level has been finalised
+            lastJumpedPos = maxReachable;     // so just move to that maxReachable position
+            jumps++;                          // and increment the level
+            // NOTE: jump^ only gets updated after we iterate all possible jumps from previous level
+            //       This ensures jumps will only store minimum jump required to reach lastJumpedPos
+        }
+        i++;
+    }
+    return jumps;
+}
+
 // greedy approach
 var jump = function (nums) {
     let jumpCount = 0, current = -1, next = 0;
