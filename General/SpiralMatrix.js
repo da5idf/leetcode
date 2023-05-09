ede68de81c469b021b24ee8ce25e02f851d88645
da5idf <devda5idf@gmail.com>
2023-05-09 09:33:50 -0400

    spiral order

--- General/SpiralMatrix.js
+++ General/SpiralMatrix.js
@@ -1,3 +1,50 @@
+// from ChatGPT
+// A little verbose, but easier to read IMO
+var spiralOrder = function (matrix) {
+    let result = [];
+    if (matrix.length === 0) {
+        return result;
+    }
+
+    let rowBegin = 0;
+    let rowEnd = matrix.length - 1;
+    let colBegin = 0;
+    let colEnd = matrix[0].length - 1;
+
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+        // Traverse right
+        for (let i = colBegin; i <= colEnd; i++) {
+            result.push(matrix[rowBegin][i]);
+        }
+        rowBegin++;
+
+        // Traverse down
+        for (let i = rowBegin; i <= rowEnd; i++) {
+            result.push(matrix[i][colEnd]);
+        }
+        colEnd--;
+
+        // Traverse left
+        if (rowBegin <= rowEnd) {
+            for (let i = colEnd; i >= colBegin; i--) {
+                result.push(matrix[rowEnd][i]);
+            }
+        }
+        rowEnd--;
+
+        // Traverse up
+        if (colBegin <= colEnd) {
+            for (let i = rowEnd; i >= rowBegin; i--) {
+                result.push(matrix[i][colBegin]);
+            }
+        }
+        colBegin++;
+    }
+
+    return result;
+}
+
+/*
 var spiralOrder = function (matrix) {
 
     let ROWS = matrix.length;
@@ -29,3 +76,4 @@ var spiralOrder = function (matrix) {
 
 let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 console.log(spiralOrder(matrix));
+*/
